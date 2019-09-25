3236e36afb0a362cb8dddeb50c75284927cf00a9
Sathish <vsk7600@gmail.com>
2019-09-26 02:01:18 +0530

    url change

--- app/src/main/java/com/starwars/android/Constants.kt
+++ app/src/main/java/com/starwars/android/Constants.kt
@@ -1,8 +1,8 @@
 package com.starwars.android
 
 object Constants {
-    val BASE_URL = "http://192.168.43.128:3000"
-    //    val BASE_URL = "http://ec2-13-233-158-146.ap-south-1.compute.amazonaws.com"
+    //    val BASE_URL = "http://192.168.43.128:3000"
+    val BASE_URL = "http://ec2-13-233-158-146.ap-south-1.compute.amazonaws.com"
     val API_BASE_URL = BASE_URL + "/api/"
     val IMAGE_ASSETS_URL = BASE_URL + "/images/"
     val DB_NAME = "starwars-db"
